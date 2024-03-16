6cde475f4162b9bd8de4d28d8391781748659f4f
eurorondon03 <eurorondon03@gmail.com>
2024-03-16 18:27:31 -0400

    first commit

--- app/page.js
+++ app/page.js
@@ -2,13 +2,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
+    <div
+      className=""
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        height: "100vh",
+        padding: 0,
+        margin: 0,
+      }}
+    >
       <h2>
         Go to `app/layout.js` to see how to implement Facebook Pixel in Next.js
         13 with the App Router
       </h2>
       <h2>If you want to see old implementation, go to `_pages/index.js`</h2>
       <Link href="/about">About page</Link>
-    </>
+    </div>
   );
 }
