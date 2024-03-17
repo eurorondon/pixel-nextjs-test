2026b77d701ab1073948ad7871f205a975a7ec1c
eurorondon03 <eurorondon03@gmail.com>
2024-03-16 21:26:57 -0400

    add fb token

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,12 @@ import { FacebookPixel } from "./components";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="facebook-domain-verification"
+          content="hhnarhtokejdum1dvr2n2lntkac6t2"
+        />
+      </Head>
       <body>
         {children}
         <FacebookPixel />
